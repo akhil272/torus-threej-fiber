36ccea0245ff2d3bcb7268006bd9f84dbbe15a90
akhil272 <akhil272@hotmail.com>
2024-02-26 11:31:15 +0530

    fixed type errors

--- components/Model.tsx
+++ components/Model.tsx
@@ -2,13 +2,16 @@ import React, { useRef } from "react";
 import { useGLTF, Text, MeshTransmissionMaterial } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
 import { useControls } from "leva";
+import { Mesh } from "three";
 
 const Model = () => {
-  const mesh = useRef();
+  const mesh = useRef<Mesh>(null!);
   const { nodes } = useGLTF("/medias/torus2.glb");
   const { viewport } = useThree();
   useFrame(() => {
-    mesh.current.rotation.x += 0.02;
+    if (mesh.current) {
+      mesh.current.rotation.x += 0.02;
+    }
   });
   const materialProps = useControls({
     thickness: { value: 0.2, min: 0, max: 3, step: 0.05 },
