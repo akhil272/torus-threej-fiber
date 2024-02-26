4d6c96774196651ea2ef3085102bf2d5344e611c
akhil272 <akhil272@hotmail.com>
2024-02-26 11:42:59 +0530

    added labelings

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,9 @@ const Home = () => {
   return (
     <div className="h-screen relative">
       <Scene />
+      <div className="absolute font-mono bottom-0 bg-black w-full text-white flex justify-center">
+        <span>developed by akhil272</span>
+      </div>
     </div>
   );
 };
